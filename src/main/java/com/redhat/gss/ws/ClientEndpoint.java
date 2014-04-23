ec7a3c4bc8d62d8488e8069c8bf8f32f04b880bf
Kyle Lape <kyle.lape@redhat.com>
2014-04-23 15:21:05 -0700

    Adding note to indicate the endopint won't be problematic

--- src/main/java/com/redhat/gss/ws/ClientEndpoint.java
+++ src/main/java/com/redhat/gss/ws/ClientEndpoint.java
@@ -2,6 +2,12 @@ package com.redhat.gss.ws;
 
 import org.jboss.logging.Logger;
 
+/**
+ * Because this is a CXF endpoint, any client buses created within the
+ * endpoint's invocation will be cleaned up by CXF after the endpoint method has
+ * returned.  Therefore it will not exhibit the same memory issues seen if
+ * <code>ClientServlet</code> was invoked instead.
+ */
 @javax.jws.WebService
 public class ClientEndpoint
 {
